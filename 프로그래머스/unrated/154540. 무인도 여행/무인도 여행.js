c5d31a611a369d38856dbf2afcbd4fb08326001a
Hyeok <lazypl82@gmail.com>
2023-07-20 17:38:35 +0900

    [unrated] Title: 무인도 여행, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
@@ -1,68 +1,37 @@
 function solution(maps) {
-  const arr = [];
+    const arrMap = maps.map((string) => string.split(""));
+    const rowLength = arrMap.length - 1;
+    const columnLength = arrMap[0].length - 1;
+    const dRow = [1, 0, -1, 0];
+    const dCol = [0, 1, 0, -1];
+    const result = [];
 
-  const vis = maps.map((e) => {
-    e = [...e];
+    const bfs = (row, col, count) => {
+        if (row > rowLength || col > columnLength || row < 0 || col < 0) {
+            return count;
+        }
+        if (arrMap[row][col] === "X") {
+            return count;
+        }
 
-    e = e.map((i) => {
-      if (i !== "X") return false;
-      else return "X";
-    });
+        count += Number(arrMap[row][col]);
+        arrMap[row][col] = "X";
 
-    return e;
-  });
+        for (let i = 0; i < 4; i++) {
+            count = bfs(row + dRow[i], col + dCol[i], count);
+        }
 
-  vis.map((e, y) => {
-    e.map((i, x) => {
-      if (!i) {
-        arr.push(Island(maps, [y, x], vis));
-      }
-    });
-  });
+        return count;
+    };
 
-  arr.sort((a, b) => a - b);
-
-  if (!arr.length) {
-    return [-1];
-  } else return arr;
-}
-
-function Island(maps = [], start = [], vis = []) {
-  let day = 0;
-
-  dirs = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  let queue = [];
-  queue.push([start[0], start[1]]);
-  vis[queue[0][0]][queue[0][1]] = true;
-
-  while (queue.length) {
-    const cur_y = queue[0][0];
-    const cur_x = queue[0][1];
-
-    day += Number(maps[cur_y][cur_x]);
-
-    for (let i = 0; i < 4; i++) {
-      const dy = cur_y + dirs[i][0];
-      const dx = cur_x + dirs[i][1];
-
-      if (
-        maps[dy] &&
-        maps[dy][dx] &&
-        maps[dy][dx] !== "X" &&
-        vis[dy][dx] !== true
-      ) {
-        vis[dy][dx] = true;
-        queue.push([dy, dx]);
-      }
+    for (let i = 0; i <= rowLength; i++) {
+        for (let j = 0; j <= columnLength; j++) {
+            if (arrMap[i][j] === "X") {
+                continue;
+            }
+            result.push(bfs(i, j, 0));
+        }
     }
-    queue.shift();
-  }
 
-  return day;
+    return result.length ? result.sort((a, b) => a - b) : [-1];
 }
