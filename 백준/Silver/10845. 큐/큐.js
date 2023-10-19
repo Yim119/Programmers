0e56b81eed46d1c2aabe88ca380644563d386852
Hyeok <lazypl82@gmail.com>
2023-10-19 16:25:52 +0900

    [Silver IV] Title: 큐, Time: 192 ms, Memory: 12376 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.shift();
+
+const arr = [];
+const result = [];
+
+input.forEach((e) => {
+  const item = e.trim();
+  switch (true) {
+    case item.includes("push"):
+      const temp = item.split(" ");
+      arr.push(parseInt(temp[1].trim()));
+      break;
+    case item === "pop":
+      const node = arr.length ? arr.shift() : -1;
+      result.push(node);
+      break;
+    case item === "size":
+      result.push(arr.length);
+      break;
+    case item === "empty":
+      result.push(arr.length ? 0 : 1);
+      break;
+    case item === "back":
+      const top = arr.length ? arr[arr.length - 1] : -1;
+      result.push(top);
+      break;
+    case item === "front":
+      result.push(arr[0] || -1);
+      break;
+  }
+});
+
+console.log(result.join("\n"));
